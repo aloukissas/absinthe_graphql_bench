756411d09dba90d854f0b9c55542c7881940954d
Alex Loukissas <aloukissas@gmail.com>
2024-10-29 07:37:03 -0700

    no logs

--- config/config.exs
+++ config/config.exs
@@ -27,6 +27,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :absinthe, log: false
+config :phoenix, :logger, false
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
